baec16ceafd4cb9dd3639a4b134b9fac1062ae08
Tom Dale <tom@tomdale.net>
2017-03-20 16:54:44 -0400

    Bump glimmer compiler

--- lib/broccoli/glimmer-template-precompiler.ts
+++ lib/broccoli/glimmer-template-precompiler.ts
@@ -1,6 +1,11 @@
 import * as Filter from 'broccoli-persistent-filter';
 import * as compiler from '@glimmer/compiler';
 
+interface TemplateMeta {
+  '<template-meta>': true;
+  specifier: string;
+}
+
 class GlimmerTemplatePrecompiler extends Filter {
   extensions = ['hbs'];
   targetExtension = 'js';
@@ -13,12 +18,13 @@ class GlimmerTemplatePrecompiler extends Filter {
 
   processString(content, relativePath) {
     let specifier = getTemplateSpecifier(this.options.rootName, relativePath);
-    return 'export default ' + compiler.precompile(content, { meta: { specifier } }) + ';';
+    return 'export default ' + compiler.precompile<TemplateMeta>(content, { meta: { specifier, '<template-meta>': true } }) + ';';
   }
 }
 
 function getTemplateSpecifier(rootName, relativePath) {
   let path = relativePath.split('/');
+  let prefix = path.shift();
 
   // TODO - should use module map config to be rigorous
   if (path[path.length - 1] === 'template.hbs') {

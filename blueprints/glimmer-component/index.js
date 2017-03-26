5f66d224c031f2a574351a3f3705ad029e4adcd1
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-03-26 17:12:58 -0400

    Add "glimmer-component" blueprint to the project
    Enables us to use "ember generate glimmer-component foo-bar" in projects that depend on "glimmer-application-pipeline"

---
+++ blueprints/glimmer-component/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const stringUtils = require('ember-cli-string-utils');
+const validComponentName = require('ember-cli-valid-component-name');
+const normalizeEntityName = require('ember-cli-normalize-entity-name');
+
+module.exports = {
+  description: 'Generates a component. Name must contain a hyphen.',
+
+  normalizeEntityName(entityName) {
+    return validComponentName(normalizeEntityName(entityName));
+  },
+
+  fileMapTokens() {
+    return {
+      __name__(options) {
+        return options.dasherizedModuleName;
+      },
+    };
+  },
+
+  locals(options) {
+    let className = stringUtils.classify(options.entity.name);
+    return { className };
+  },
+};

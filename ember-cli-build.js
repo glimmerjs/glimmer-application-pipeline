accabed6033b0dca2111cc8cb72e047c50b87f9e
Robert Jackson <rjackson@linkedin.com>
2017-04-03 17:47:13 -0400

    Clear warnings for externals with Rollup.
    Silences warnings in the console for now, but in the long term we
    should update @glimmer/build to allow passing rollup-plugin-node-builtins
    and rollup-plugin-node-resolve (and possibly rollup-plugin-commonjs).

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,5 +3,31 @@
 const build = require('@glimmer/build');
 
 module.exports = function() {
-  return build();
+  return build({
+    external: [
+      'broccoli-config-loader',
+      'broccoli-config-replace',
+      'broccoli-funnel',
+      'broccoli-concat',
+      'path',
+      'fs',
+      'lodash.defaultsdeep',
+      'broccoli-typescript-compiler',
+      'exists-sync',
+      'broccoli-merge-trees',
+      'broccoli-sass',
+      'broccoli-asset-rev',
+      'broccoli-uglify-sourcemap',
+      '@glimmer/resolution-map-builder',
+      '@glimmer/resolver-configuration-builder',
+      'broccoli-rollup',
+      'rollup-plugin-node-resolve',
+      'rollup-plugin-babel',
+      'broccoli-persistent-filter',
+      '@glimmer/compiler',
+      'broccoli-source',
+      'heimdalljs-logger',
+      'broccoli-stew'
+    ]
+  });
 }

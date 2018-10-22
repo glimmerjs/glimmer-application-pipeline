3beb74b3b59e74799c840ab040de3e85c176cf4c
Jonathan Jackson <jonathan.jackson1@me.com>
2018-10-22 10:43:01 -0400

    Update component-test blueprint to use render import and testContext data passing

--- blueprints/glimmer-component-test/files/src/ui/components/__name__/component-test.ts
+++ blueprints/glimmer-component-test/files/src/ui/components/__name__/component-test.ts
@@ -1,4 +1,4 @@
-import { setupRenderingTest } from '@glimmer/test-helpers';
+import { setupRenderingTest, render } from '@glimmer/test-helpers';
 import hbs from '@glimmer/inline-precompile';
 
 const { module, test } = QUnit;
@@ -7,7 +7,26 @@ module('Component: <%= componentName %>', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    await this.render(hbs`<<%= componentName %> />`);
+    /*
+     * You may pass data into the component through arguments set on the
+     * `testContext`
+     *
+     * For example:
+     *
+     * ```
+     * this.foo = { foo: '123' };
+     *
+     * await render(hbs`<<%= componentName %> @foo={{this.foo}} />`)
+     *
+     * // or
+     *
+     * this.foo = 'bar';
+     * await render(hbs`<p>{{this.foo}}</p>`);
+     *
+     * assert.dom('p').text('bar');
+     * ```
+     */
+    await render(hbs`<<%= componentName %> />`);
     assert.ok(this.containerElement.querySelector('div'));
   });
 });
